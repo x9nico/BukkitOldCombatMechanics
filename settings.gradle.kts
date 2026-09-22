9e0b8ecfe81c768a6065afa5cf3980ff653ad4eb
x9nico <xnico2809@gmail.com>
2026-09-22 20:35:26 +0200

    26.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
